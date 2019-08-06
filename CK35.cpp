ee2cc6611b80143c333f6d819f9b58ddf4b51364
Naveen_Kumar24 <51779899+naveenkumar2405@users.noreply.github.com>
2019-08-06 23:20:46 +0530

    Update CK35.cpp

--- CK35.cpp
+++ CK35.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
-#include <string.h>
+#include <string>
+
 using namespace std;
 
 int main()
 {
   string str;
   int count = 0;
-
+  
   getline(cin, str);
-
-  for(int i = 0; str[i] != '\0'; i++)
+  
+  for(int ind = 0; str[ind] != '\0'; ind++)
   {
-      if( str[i] != ' ' && (str[i] < '0' || str[i] > '9') && (str[i] < 'a' || str[i] > 'z') && (str[i] < 'A' || str[i] > 'Z') )
+      if(str[ind] >= '0' && str[ind] <= '9')
         count++;
   }
-
+  
   cout << count;
-
+  
   return 0;
 }
